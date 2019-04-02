aae60a25b9c0424ff982a392ae503340a3bac9ac
Tessa Sullivan and Zema Kebede <tessa@>
2019-04-02 14:53:26 -0700

    tells user whether or not word entered is a palindrome

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,33 @@
+function testPalindrome(word){
+  // split word at the halfway point
+  // reverse 2nd half and compare
+  var splitWord = word.split("");
+  var firstHalf = splitWord.slice(0, splitWord.length/2);
+  var secondHalf = splitWord.reverse().slice(0, splitWord.length/2);
+
+  if (firstHalf.toString() === secondHalf.toString()) {
+    return true;
+
+  } else {
+    return false;
+  }
+
+
+}
+
+
 $(document).ready(function(){
   $("form#palindrome").submit(function(event){
     event.preventDefault();
     var word = $("input#wordToTest").val();
-    console.log(word);
-    $("#result").append(word);
+    var result = testPalindrome(word);
+    if (result) {
+      $("#result").empty;
+      $("#result").append("This is a palindrome");
+    } else {
+      $("#result").empty;
+      $("#result").append("This is NOT a palindrome");
+    }
   });
 
 });
